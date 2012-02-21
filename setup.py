328799dad2e7dd6c911132fcf5a0acef76ccfd1f
Sean O'Connor <sean@focuslab.io>
2012-02-20 20:23:16 -0500

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError, err:
+    from distutils.core import setup
+
+from dj_albertson import VERSION
+
+setup(
+    name='django-albertson',
+    version=".".join(map(str, VERSION)),
+    description="Integration library for Django and Albertson.",
+    author="Sean O'Connor",
+    author_email="sean@focuslab.io",
+    url="https://github.com/FocusLab/django-albertson",
+    packages=['dj_albertson'],
+    license="BSD",
+    long_description=open('README.md').read(),
+    install_requires=['albertson', 'django'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    test_suite='nose.collector',
+)
